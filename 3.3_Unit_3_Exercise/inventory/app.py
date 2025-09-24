a217cc0c439bbed84a0a554298acefb8bb5c1ce3
Mehrab B <mbucktow@gmail.com>
2025-09-24 10:06:07 +0100

    Added Step 15 - save and load items on disk

--- 3.3_Unit_3_Exercise/inventory/app.py
+++ 3.3_Unit_3_Exercise/inventory/app.py
@@ -9,28 +9,48 @@
 #if __name__ == "__main__":
 #    app.run(debug=True)
 
+import json
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# Step 4: seed items
-items = [
-    {
-        "id": 1,
-        "name": "Laptop",
-        "manufacturer": "Dell",
-        "sku": "LAP123",
-        "price": 120000  # pennies = £1200.00
-    },
-    {
-        "id": 2,
-        "name": "Mouse",
-        "manufacturer": "Logitech",
-        "sku": "MOU456",
-        "price": 2000  # pennies = £20.00
-    }
-]
+DATA_FILE = "items.json"
 
+# Step 15 Load items on startup
+# Load and save data ---
+def load_items():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    return []
+
+def save_items():
+    with open(DATA_FILE, "w") as f:
+        json.dump(items, f, indent=2)
+
+items = load_items()
+
+
+# Step 4: seed items
+if not items:
+    items = [
+        {
+            "id": 1,
+            "name": "Laptop",
+            "manufacturer": "Dell",
+            "sku": "LAP123",
+            "price": 120000  # pennies = £1200.00
+        },
+        {
+            "id": 2,
+            "name": "Mouse",
+            "manufacturer": "Logitech",
+            "sku": "MOU456",
+            "price": 2000  # pennies = £20.00
+        }
+    ]
+    save_items()
 
 # Step 6 GET all items
 @app.route("/items", methods=["GET"])
@@ -61,6 +81,7 @@ def create_item():
         "price": data.get("price")
     }
     items.append(new_item)
+    save_items()
     return jsonify(new_item), 201
 
 # Step 11 Modify an existing item
@@ -77,6 +98,7 @@ def update_item(item_id):
             item["manufacturer"] = data.get("manufacturer", item["manufacturer"])
             item["sku"] = data.get("sku", item["sku"])
             item["price"] = data.get("price", item["price"])
+            save_items()
             return jsonify(item)
 
     return jsonify({"error": "Item not found"}), 404
@@ -88,6 +110,7 @@ def delete_item(item_id):
         if item["id"] == item_id:
             items.remove(item)
             return jsonify({"message": "Item deleted successfully"})
+            save_items()
     return jsonify({"error": "Item not found"}), 404
 
 
